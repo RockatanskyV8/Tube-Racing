bdef5f79ffb8fa04119fa3e477779c16e722aa60
Lucas Santos Rosa <luccasanttoz@gmail.com>
2018-02-13 22:43:59 -0200

    Excluindo pistas

--- Gravidade/Assets/Scripts/Gerador.cs
+++ Gravidade/Assets/Scripts/Gerador.cs
@@ -7,23 +7,31 @@ public class Gerador : MonoBehaviour {
 	public GameObject[] pistaPrefabs;
 	
 	private Transform carro;
-	private float spawnZ = 0.0f;
-	private float pistaLength = 20f;
+	private float spawnZ = -12.0f;
+	private float pistaLength = 19.5f;
+	private float safeZone = 15.0f;
 	private int quantPistas = 3;
+	
+	private List<GameObject> pistasAtivadas;
+	private int ultimoPrefab = 0;
 	
 	// Use this for initialization
 	void Start () {
+		pistasAtivadas = new List<GameObject>();
 		carro = GameObject.FindGameObjectWithTag("Carro").transform;
 		for(int i = 0; i < quantPistas; i++)
 		{
 			Summoner();
+			
 		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if( carro.position.z > (spawnZ - quantPistas * pistaLength) ){
-			Summoner();
+		if( carro.position.z - safeZone > (spawnZ - quantPistas * pistaLength) ){
+			//Summoner();
+			Randomer();
+			Unsommoner();
 		}
 	}
 	
@@ -34,5 +42,26 @@ public class Gerador : MonoBehaviour {
 		go.transform.SetParent(transform);
 		go.transform.position = Vector3.forward * spawnZ;
 		spawnZ += pistaLength;
+		pistasAtivadas.Add(go);
+	}
+	
+	void Randomer(int prefabIndex = -1)
+	{
+		GameObject go;
+		go = Instantiate(pistaPrefabs[Random.Range(0, pistaPrefabs.Length)]) as GameObject;
+		go.transform.SetParent(transform);
+		go.transform.position = Vector3.forward * spawnZ;
+		
+		Quaternion rotacao = go.transform.rotation;
+		rotacao.eulerAngles = new Vector3(0, 0, Random.Range(0, 360));
+		go.transform.localRotation = rotacao;
+		spawnZ += pistaLength;
+		pistasAtivadas.Add(go);
+	}
+	
+	void Unsommoner()
+	{
+		Destroy(pistasAtivadas[0]);
+		pistasAtivadas.RemoveAt(0);
 	}
 }
